27425ca5102bc2cfda57b136b1f1129f24f30e33
Stefanno Rossel <stefannorp1510@gmail.com>
2024-04-06 15:11:14 -0500

    a

--- Semana09Sesion02/d19423/querys.sql
+++ Semana09Sesion02/d19423/querys.sql
@@ -229,8 +229,7 @@ ORDER BY Customers.CustomerName;
 
 SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
 FROM Customers A, Customers B
-WHERE A.CustomerID <> B.CustomerID
-AND A.City = B.City
+WHERE A.CustomerID <> B.CustomerID AND A.City = B.City
 ORDER BY A.City;
 
 
