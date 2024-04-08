ac3449718e277139fd1d50a4dd1e88b1cb777c4f
Jhens <u18310128@utp.edu.pe>
2024-04-07 21:54:21 -0500

    Hackaton 11

--- Hackaton10/SV75439808/index.js
+++ Hackaton10/SV75439808/index.js
@@ -1,20 +1,7 @@
-const { MongoClient } = require("mongodb");
-
-const uri = "mongodb+srv://u18310128:Is5FsYXeClIUqJwV@cluster0.l8ehbli.mongodb.net/";
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    await client.connect();
-    const db = client.db("sample_mflix");
-    const collection = db.collection("movies");
-
-    // Encuentra el primer documento de la colección.
-    const first = await collection.findOne();
-    console.log(first);
-  } finally {
-    // Cerrar la conexión de la base de datos cuando termine o se produzca un error
-    await client.close();
-  }
-}
-run().catch(console.error);
+let MongoClient = require("mongodb").MongoClient;
+let url =
+  "mongodb+srv://u18310128:Is5FsYXeClIUqJwV@cluster0.l8ehbli.mongodb.net/SV75439808";
+MongoClient.connect(url, function (err, deb) {
+  console.log("Connected");
+  db.close();
+});
