bb1748ac587c7dee81c545fc9880d61b645cb213
helmutpacheco <helmutinp@gmail.com>
2024-04-10 22:06:07 -0500

    semana 12 sesion 2

--- Semana12Sesion02/SV41168130/index.js
+++ Semana12Sesion02/SV41168130/index.js
@@ -1,9 +1,26 @@
 var http = require('http');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-  
-    
+http.createServer(function (req, res) {    
+    console.log(req.url);
+    switch (req.url) {        
+        case '/':
+            console.log(req.url);
+            fs.readFile('index.html', function(err, data) {
+                if(err){
+                    res.writeHead(404, {'Content-Type': 'text-html'});
+                    return res.end('404 Not found');
+                }
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+            break;
+        case '/index':
+            break;
+        default:
+            break;
+    }
 
-    res.end('Hello World!');
+
 }).listen(8080);
