2ddb6e44401a4a389bebf4df4e25e0917736d463
Roberto Pineda <rpineda@x-codec.net>
2024-04-17 20:07:12 -0500

    clase 16042024 segunda parte

---
+++ Semana13Sesion01/d19423/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyparser = require("body-parser");
+const app = express()
+const port = 3000
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    let query = req.query;
+    console.log(query);
+    res.sendStatus(201);//.sendFile(__dirname +'/uploads/gundam.png')
+    //res.send(`Hello World! ${query.nombre}`)
+  })
+
+app.get('/:id', (req, res) => {
+    let params = req.params;
+    console.log(params);
+    res.send(`Hello World! ${params.id}`)
+  })
+
+app.post('/user/:id', (req, res) => {
+    let params = req.params;
+    let data = req.body
+    console.log(req);
+    
+    //res.send(`Hello World! ${data.nombre}`)
+    //res.status(301).location('http://www.x-codec.net')
+    
+    res.end()
+  })
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
