044a66542dd8b2cc309d1b40cc763f6452ad3bdc
RomelCarlof <rib2419882488@gmail.com>
2024-04-19 15:26:57 -0500

    Tercer Cambio

--- Hackaton11/SV45213867/index.js
+++ Hackaton11/SV45213867/index.js
@@ -1,10 +1,22 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-const sequelize = new Sequelize('sqlite::memory:');
+//const sequelize = new Sequelize('sqlite::memory:');
+
+const sequelize = new Sequelize(
+ 'hello_world_db',
+ 'root',
+ '',
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+);
 
 const User = sequelize.define('User', {
     username: DataTypes.STRING,
     birthday: DataTypes.DATE,
+    profesion: DataTypes.STRING,
+    pais: DataTypes.STRING,
 });
 
 await sequelize.sync({ force: true });
@@ -12,6 +24,8 @@ console.log('All models were synchronized successfully.');
 const jane = await User.create({
     username: 'janedoe',
     birthday: new Date(1980, 6, 20),
+    profesion: 'Ingeniero de Sistema',
+    pais: 'Pais',
 });
 
 const users = await User.findAll();
