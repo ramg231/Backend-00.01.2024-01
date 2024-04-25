70f69fb573e4c58450ebc2c53ad4525dd6a821b5
Roberto Pineda <rpineda@x-codec.net>
2024-04-24 21:05:22 -0500

    Clase 24042024 segunda parte

--- Semana14Sesion02/d19423/ParteUno/index.js
+++ Semana14Sesion02/d19423/ParteUno/index.js
@@ -8,6 +8,16 @@ const server = createServer(app);
 const io = new Server(server);
 const https = require('https');
 
+const options = {
+	method: 'GET',
+	hostname: 'moviesdatabase.p.rapidapi.com',
+	port: null,
+	path: '/titles/x/upcoming',
+	headers: {
+		'X-RapidAPI-Key': '73d70d2c28msh7f79106bce6c25ep19a96ajsn943644966186',
+		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+	}
+};
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
@@ -17,37 +27,37 @@ io.on('connection', (socket) => {
     console.log('a user connected');
     socket.broadcast.emit('hi');
     socket.on('chat message', (msg) => {
-        // if(msg == "clima"){
-        //     const req = https.request(options, function (res) {
-        //         const chunks = [];
+        if(msg == "cine"){
+            const req = https.request(options, function (res) {
+                const chunks = [];
             
-        //         res.on('data', function (chunk) {
-        //             chunks.push(chunk);
-        //         });
+                res.on('data', function (chunk) {
+                    chunks.push(chunk);
+                });
             
-        //         res.on('end', function () {
-        //             const body = Buffer.concat(chunks);
-        //             let jsonData = JSON.parse(body.toString()).results;
-        //             console.log(jsonData);
-        //             // Mostrar solo los títulos de las películas y la fecha de estreno
-        //             arrRespuesta = [];
-        //             for (let i=0;i<jsonData.length;i++){
-        //              //   console.log(jsonData[i].primaryImage["url"]);
-        //              objImagen = jsonData[i].primaryImage;
-        //                 let info = {
-        //                     titulo: jsonData[i].titleText.text,
-        //                     estreno: new Date(  jsonData[i].releaseDate.year, jsonData[i].releaseDate.month,jsonData[i].releaseDate.day),
-        //                     portada: objImagen
-        //                 }
-        //                 arrRespuesta.push(info);
+                res.on('end', function () {
+                    const body = Buffer.concat(chunks);
+                    let jsonData = JSON.parse(body.toString()).results;
+                    console.log(jsonData);
+                    // Mostrar solo los títulos de las películas y la fecha de estreno
+                    arrRespuesta = [];
+                    for (let i=0;i<jsonData.length;i++){
+                     //   console.log(jsonData[i].primaryImage["url"]);
+                     objImagen = jsonData[i].primaryImage;
+                        let info = {
+                            titulo: jsonData[i].titleText.text,
+                            estreno: new Date(  jsonData[i].releaseDate.year, jsonData[i].releaseDate.month,jsonData[i].releaseDate.day),
+                            portada: objImagen
+                        }
+                        arrRespuesta.push(info);
                         
-        //             }
-        //             console.log(arrRespuesta);
-        //             io.emit("chat message",JSON.stringify( arrRespuesta));
-        //         });
-        //     });
-        //     req.end();
-        // }
+                    }
+                    console.log(arrRespuesta);
+                    io.emit("chat message",JSON.stringify( arrRespuesta));
+                });
+            });
+            req.end();
+        }
         io.emit('chat message', msg);
         console.log('message: ' + msg);
     });
