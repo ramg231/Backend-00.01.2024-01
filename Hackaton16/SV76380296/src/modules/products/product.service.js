0b510d8fb9be2ed957b7918b5d879098c0329bdb
PR2004 <ramoslirion2021@gmail.com>
2024-05-12 21:54:01 -0500

    Hackaton 16

---
+++ Hackaton16/SV76380296/src/modules/products/product.service.js
@@ -0,0 +1,24 @@
+const { request, response } = require("express");
+const { createProductStripe } = require("../../services/stripe/stripe.services")
+const ProductModel = require("./product.entity")
+
+const createProduct = async (req = request, res = response) =>{
+    //validate admin role
+    
+    const data = req.body;
+
+    console.log(req.body)
+
+    const productStripe = await createProductStripe(data);
+
+    const newProduct = await ProductModel.create({
+        ...data,
+        stripeId: productStripe.id
+    })
+
+    return res.json(newProduct)
+};
+
+module.exports = {
+    createProduct,
+}
